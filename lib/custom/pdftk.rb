67e4bb2e352243ff7ff5f69523c5500acd6d2573
Jared Brown <jaredbrown@gmail.com>
2013-05-07 20:50:37 -1000

    Host pdftk on S3

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -6,7 +6,7 @@ class Pdftk < BaseCustom
     "pdftk"
   end
   def source_url
-    "https://dl.dropboxusercontent.com/u/16498212/pdftk.tar.gz"
+    "https://s3.amazonaws.com/pdftk-source/pdftk.tar.gz"
   end
   def used?
     File.exist?("#{build_path}/bin/pdftk") && File.exist?("#{build_path}/bin/lib/libgcj.so.12")
